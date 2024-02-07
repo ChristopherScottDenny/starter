02489679be773e78f4cce0ada9cc161d81d86e42
Christopher Denny <christopherscottdenny@gmail.com>
2024-02-07 15:10:06 -0800

    Add push shortcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,4 +1,7 @@
 return {
   "tpope/vim-fugitive",
-  keys = { { "<leader>gf", "<cmd>Git<cr>", desc = "Git Fugitive" } },
+  keys = {
+    { "<leader>gf", "<cmd>Git<cr>", desc = "Git Fugitive" },
+    { "<leader>gp", "<cmd>Git push<cr>", desc = "Git Push Upstream" },
+  },
 }
